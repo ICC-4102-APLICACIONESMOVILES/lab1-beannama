4ffc695e9f323652a6817fb3b7d7f086c2a110d7
beannama <ben.nar.95@gmail.com>
2019-03-25 11:35:38 -0300

    prueba

---
+++ app/src/main/java/com/example/lab01_naranjo_benjamin/Activity2.kt
@@ -0,0 +1,14 @@
+package com.example.lab01_naranjo_benjamin
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class Activity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
---
+++ app/src/main/java/com/example/lab01_naranjo_benjamin/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.lab01_naranjo_benjamin
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val get_log_request = 1
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val intent = Intent(this, Activity2::class.java)
+        startActivityForResult(intent, get_log_request)
+    }
+
+
+}
